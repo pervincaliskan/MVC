421fa624dad503b3711f58c579c96712815ac333
Pervin Çalışkan <pervincaliskan01@hotmail.com>
2022-04-03 17:32:24 +0300

    Doğruluk zorunluluk kontrol örneği eklendi

---
+++ ValidationControls/Controllers/UyeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ValidationControls.Controllers
+{
+    public class UyeController : Controller
+    {
+        // GET: Uye
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Yeni()
+        {
+            return View();
+        }
+    }
+}
---
+++ ValidationControls/Models/Uyebil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;   // Displayname icin eklemeyi unutma
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ValidationControls.Models
+{
+    public class Uyebil
+    {
+        public int Id { get; set; }
+
+
+        [DisplayName("Kullanıcı adı")]
+
+        public string kullaniciAd { get; set; }
+
+        [DisplayName("Kullanıcı Şifre")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Şifre bilgisi minimum 6 karakter olmalıdır")]
+
+
+        public string kullanicisifre { get; set; }
+
+        [DisplayName("Şifre Tekrarı")]
+        [Compare("kullanicisifre")]
+        public string sifre2 { get; set; }
+
+
+        [DisplayName("Email")]
+        [EmailAddress()]
+        [MaxLength(25, ErrorMessage = "Şifre bilgisi max 25 karakter olmalıdır")]
+        public string email { get; set; }
+
+
+    }
+}
