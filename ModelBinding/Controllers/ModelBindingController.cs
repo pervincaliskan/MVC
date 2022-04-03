79886e09142b4efec72815bb1d12458dfd7c537c
Pervin Çalışkan <pervincaliskan01@hotmail.com>
2022-04-03 18:05:24 +0300

    ModelBinding örneği eklendi

---
+++ ModelBinding/Controllers/ModelBindingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelBinding.Controllers
+{
+    public class ModelBindingController : Controller
+    {
+        // GET: ModelBinding
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Kayit()
+
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Kayit(string urunad)
+
+        {
+            return View();
+        }
+    }
+}
